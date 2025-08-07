cf3760fabf4f20c2cdaf940c284ff104d7f52d26
Dominique RIGHETTO <righettod@users.noreply.github.com>
2025-08-07 15:06:30 +0200

    Update add-protections.sh

--- add-protections.sh
+++ add-protections.sh
@@ -3,7 +3,7 @@
 grep -hIPro '(?i)https?://[a-z0-9\-.]+' * | cut -d '/' -f3 | sort -u > /tmp/hosts.txt
 while IFS= read -r domain; do
   printf "%s\t%s\n" "127.0.0.1" "$domain" >> /etc/hosts
-done < hosts.txt
+done < /tmp/hosts.txt
 rm /tmp/hosts.txt
 # Add a CSP to handle the browser level
 sed -i 's/<\/head>/<meta http-equiv="Content-Security-Policy" content="default-src '\''self'\'' '\''unsafe-inline'\''; form-action '\''self'\''; base-uri '\''self'\''; object-src '\''none'\''; frame-ancestors '\''none'\'';"\/><\/head>/g' index.html
